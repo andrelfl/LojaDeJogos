d6b3bda321c7e67f3317f4f22afb2e2b2d5d16e6
Andre Fronteira <aluno20160@ipt.pt>
2018-07-15 23:29:33 +0100

    Alteracao da route default

--- LojaDeJogos/App_Start/RouteConfig.cs
+++ LojaDeJogos/App_Start/RouteConfig.cs
@@ -14,7 +14,7 @@ namespace LojaDeJogos {
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Jogos", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
